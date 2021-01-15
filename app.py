482a730f9f56f0a7a69b5bacfd1bb8c4cd043c5c
licensetocole <70180087+licensetocole@users.noreply.github.com>
2021-01-15 11:39:34 -0800

    margin parameter added

---
+++ app.py
@@ -0,0 +1,75 @@
+import re
+from chalice import Chalice
+from chalicelib import test_order
+
+app = Chalice(app_name='botbu')
+
+@app.route('/')
+def index():
+    return {'hell0': 'world'}
+
+@app.route('/sell_stock', methods=['POST'])
+def buy_stock():
+    request = app.current_request
+    webhook_message = request.json_body
+    data = {
+        'symbol': webhook_message['ticker'],
+        'qty' : .01,
+        'side' : 'sell'
+        'tradeTYPE' ; 'MARGIN'
+    }
+    order_result, info = test_order.order(data['side'], data['qty'], data['symbol'],data['tradeTYPE']
+    if order_result:
+        return {
+            'msg': 'I sold the stock',
+            'order_id' : info,
+            'webhook_msg': webhook_message
+        }
+    return {
+            'msg': 'Failed to sell the stock',
+            'error': info,
+            'webhook_msg': webhook_message
+    }
+
+@app.route('/buy_stock', methods=['POST'])
+def buy_stock():
+    request = app.current_request
+    webhook_message = request.json_body
+    data = {
+        'symbol': webhook_message['ticker'],
+        'qty' : .01,
+        'side' : 'buy'
+        'tradeTYPE' : 'MARGIN'
+    }
+    order_result, info = test_order.order(data['side'], data['qty'], data['symbol'], data['tradeTYPE']
+    if order_result:
+        return {
+            'msg': 'I bought the stock',
+            'order_id' : info,
+            'webhook_msg': webhook_message
+        }
+    return {
+            'msg': 'Failed to buy the stock',
+            'error': info,
+            'webhook_msg': webhook_message
+    }
+
+# The view function above will return {"hello": "world"}
+# whenever you make an HTTP GET request to '/'.
+#
+# Here are a few more examples:
+#
+# @app.route('/hello/{name}')
+# def hello_name(name):
+#    # '/hello/james' -> {"hello": "james"}
+#    return {'hello': name}
+#
+# @app.route('/users', methods=['POST'])
+# def create_user():
+#     # This is the JSON body the user sent in their POST request.
+#     user_as_json = app.current_request.json_body
+#     # We'll echo the json body back to the user in a 'user' key.
+#     return {'user': user_as_json}
+#
+# See the README documentation for more examples.
+#
---
+++ test_order.py
@@ -0,0 +1,27 @@
+import kucoin.client as kuclinet
+from .api_creds import *
+
+order_ids=[]
+
+def order(data):
+    # client = kuclinet.Client(KU_API_PUBLIC, KU_API_SECRET, KU_PASSPHRASE)
+    client = kuclinet.Client(data['kucoin_api_public_key'], data['kucoin_api__secret_key'], data['kucoin_api_pass'])
+    fixed_symbol = eth_tick_fix(data['symbol'])
+    try:
+        print('\nSending order!')
+        order = client.create_market_order(fixed_symbol, data['side'], data['qty'], data['tradeTYPE'])
+        print(order)
+        order_ids.append(order)
+    except Exception as e:
+        print('Failed to place order')
+        print(e.__cause__)
+        return (False, str(e.__cause__))
+    return (True, order)
+
+def eth_tick_fix(eth_ticker):
+    if 'ETH' in eth_ticker:
+        return 'ETH-USDT'
+    return 'WRONG'
+
+if __name__=='__main__':
+    order('buy', .01, 'ETH-USDT' )
